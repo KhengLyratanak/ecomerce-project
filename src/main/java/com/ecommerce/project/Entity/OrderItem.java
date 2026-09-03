87b19fd4010e3d9c73ffde6173bca6b7ddabba3a
KhengLyratanak <khenglyratanak.gmail.com>
2026-09-03 22:47:11 +0700

    FEAT: ORDER

---
+++ src/main/java/com/ecommerce/project/Entity/OrderItem.java
@@ -0,0 +1,26 @@
+package com.ecommerce.project.Entity;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "order_Items")
+@Data
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "product_id")
+    private Long productId;
+
+    @Column(name = "qty")
+    private Long quantity;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id",nullable = false)
+    @JsonIgnore
+    private Order order;
+}
